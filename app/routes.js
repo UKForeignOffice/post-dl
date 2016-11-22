630eb5b2db66084975f7f0bb2b45f2b308c4e069
jakeholland-fco <jake.holland@gmail.com>
2016-11-22 23:14:37 +0000

    App folder upload only

---
+++ app/routes.js
@@ -0,0 +1,86 @@
+var express = require('express')
+var router = express.Router()
+var request = require('request')
+var json2csv = require('json2csv')
+var fs = require('fs')
+
+
+// Route index page
+router.get('/', function (req, res) {
+
+  res.render('index')
+})
+
+// add your routes here
+
+
+
+router.get('/afghanistan', function (req, res) {
+
+var fields = ['title', 'format', 'web_url']
+
+request('https://www.gov.uk/api/world-locations/afghanistan/organisations', function(error, response, body){
+	var results = JSON.parse(body).results
+
+	console.dir(results)
+	console.dir(results[0].title)
+
+var csv = json2csv({ data: results, fields: fields });
+
+	console.dir(csv)
+
+fs.writeFileSync('afghanistan.csv', csv)
+	console.dir('file saved');
+;
+
+	res.download ('afghanistan.csv')
+
+});
+
+router.get('/canada', function (req, res) {
+
+var fields = ['title', 'format', 'web_url']
+
+request('https://www.gov.uk/api/world-locations/canada/organisations', function(error, response, body){
+	var results = JSON.parse(body).results
+
+	console.dir(results)
+	console.dir(results[0].title)
+
+var csv = json2csv({ data: results, fields: fields });
+
+	console.dir(csv)
+
+fs.writeFileSync('canada.csv', csv)
+	console.dir('file saved');
+;
+
+	res.download ('canada.csv')
+
+});
+
+router.get('/usa', function (req, res) {
+
+var fields = ['title', 'format', 'web_url']
+
+request('https://www.gov.uk/api/world-locations/usa/organisations', function(error, response, body){
+	var results = JSON.parse(body).results
+
+	console.dir(results)
+	console.dir(results[0].title)
+
+var csv = json2csv({ data: results, fields: fields });
+
+	console.dir(csv)
+
+fs.writeFileSync('usa.csv', csv)
+	console.dir('file saved');
+;
+
+	res.download ('usa.csv');
+
+});
+
+});
+
+module.exports = router;
